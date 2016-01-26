952e3f13011bf0afad1fe1180c5c8e487b03c7e8
Nicolas Bougie <nbougie@a-018250.ups.u-psud.fr>
2016-01-26 12:15:18 +0100

    first add

---
+++ perceptron.py
@@ -0,0 +1,88 @@
+import numpy as np
+import random
+
+def data_reader(filename):
+    to_binary = {"?": 3, "y": 2, "n": 1}
+    labels = {"democrat": 1, "republican": -1}
+
+    data = []
+    for line in open(filename, "r"):
+        line = line.strip()
+
+        label = int(labels[line.split(",")[0]])
+        observation = np.array([to_binary[obs] for obs in line.split(",")[1:]] + [1])
+        data.append((label, observation))
+
+    return data
+
+
+def spam_reader(filename):
+    to_binary = {1: 1, 0: -1}
+    data = []
+    for line in open(filename, "r"):
+        line = line.strip()
+        label = to_binary[int(line.split(",")[-1])]
+        observation = [float(obs) for obs in line.split(",")[:-1] + [1.0]]
+
+        data.append((label, np.array(observation)))
+        
+    return data
+
+def moyenne_recursive(data,k):
+    if(k==1):
+        return data[0]
+    m_k_1 = moyenne_recursive(data[1::],k-1)
+    return (m_k_1+((data[k-1] - m_k_1)/k))
+
+def calculate():
+    n = 10 ** 6
+    data = 10 ** 9 + np.random.uniform(0,1,n)
+    print("moyenne : ",np.mean(data))
+    print("variance : ",np.var(data)) 
+    print("moyenne recusrive" , moyenne_recursive([1,2,3,4,5],len([1,2,3,4,5])))
+
+#calculate()
+
+
+
+#----------------------------------------------------------------- Perceptron --------------------------------------
+data = data_reader("house-votes-84.data")
+
+def getRandom(data):
+    random.seed(100)
+    random.shuffle(data)
+    train = data[:len(data)-100]
+    test = data[:100]
+    return (train,test)
+
+(train,tests) = getRandom(data) 
+   
+
+def classify(observation, poids):
+    vl = np.dot(observation,poids)
+    if(vl>=0):
+        return 1
+    return -1
+
+
+def test(corpus,poids):
+    erreur = 0.0
+    for s in corpus:
+        (value,elements) = s
+        if not classify(elements,poids)== value:
+            erreur +=1
+    return erreur/len(corpus)
+
+
+def learn(train):
+    poids = [0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for (value,elements) in train:
+        if(not classify(elements,poids) == value):
+            poids = poids + np.dot(elements,value)
+    return poids
+    
+
+print(test(tests,[25,-12, 67, -104, -43, 46, -18, -10, 45, -33, 54, -39, 43, -19, 5, -2, 55]))           
+mon_poids = learn(data)
+print(test(tests,mon_poids))  
+        
